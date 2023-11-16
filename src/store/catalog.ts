b9eee8e30e3c2fa3ce567ae42acf3a644adb602b
YoannCos <yoanncos@gmail.com>
2023-11-16 18:33:15 +0100

    add arrays of ids in catalog store

--- src/store/catalog.ts
+++ src/store/catalog.ts
@@ -5,12 +5,16 @@ import { camelCase } from "change-case/keys";
 import { Product } from "../types/products";
 
 export interface catalogState {
+  categoriesIds: number[]; //use for easy use of maps and filter (keep up to date!)
+  productsIds: number[]; // use for easy use of maps and filter (keep up to date!)
   categories: Map<number, ProductCategorie>;
   products: Map<number, Product>;
 }
 
 export const useCatalog = defineStore("catalog", {
   state: (): catalogState => ({
+    categoriesIds: [],
+    productsIds: [],
     categories: new Map(),
     products: new Map(),
   }),
@@ -18,26 +22,40 @@ export const useCatalog = defineStore("catalog", {
   actions: {
     async fetchCategories() {
       const { data: dataCategories }: { data: ProductCategorie[] } = await axiosInstance.get("/products/categories");
+
+      let categoriesId: number[] = [];
       let categoriesMap = new Map<number, ProductCategorie>();
 
       for (const categorie of dataCategories) {
+        categoriesId.push(categorie.id);
         categoriesMap.set(categorie.id, camelCase(categorie) as ProductCategorie);
       }
+
+      this.categoriesIds = categoriesId;
       this.categories = categoriesMap;
     },
 
     async fetchProducts() {
       const { data: dataProducts }: { data: Product[] } = await axiosInstance.get("/products");
+
+      let productsId: number[] = [];
       let productsMap = new Map<number, Product>();
 
       for (const product of dataProducts) {
+        productsId.push(product.id);
         productsMap.set(product.id, camelCase(product) as Product);
       }
+
+      this.productsIds = productsId;
       this.products = productsMap;
     },
 
     async fetchProductById(productId: number) {
       const { data: dataProduct }: { data: Product } = await axiosInstance.get(`/products/${productId}`);
+      if (!this.productsIds.includes(dataProduct.id)) {
+        // important : product wasn't fetch before and wasn't in id's array
+        this.productsIds.push(productId);
+      }
       this.products.set(dataProduct.id, camelCase(dataProduct) as Product);
     },
 
@@ -46,6 +64,10 @@ export const useCatalog = defineStore("catalog", {
         params: { categories: categoryId },
       });
       for (const product of dataProducts) {
+        if (!this.productsIds.includes(product.id)) {
+          // important : product wasn't fetch before and wasn't in id's array
+          this.productsIds.push(product.id);
+        }
         this.products.set(product.id, camelCase(product) as Product);
       }
     },
