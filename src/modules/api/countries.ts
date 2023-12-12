00a434ffe4a27b7e6f2dec74f5066041e76ecf04
YoannCos <yoanncos@gmail.com>
2023-12-12 14:55:50 +0100

    fix url api countries

--- src/modules/api/countries.ts
+++ src/modules/api/countries.ts
@@ -4,7 +4,7 @@ import { axiosInstanceWoo, setApiResponseStatus } from "./api";
 const countries = {
   get: async (): Promise<ApiResponseStatus> => {
     try {
-      const res = await axiosInstanceWoo.get("/data/countriesaefae");
+      const res = await axiosInstanceWoo.get("/data/countries");
       return setApiResponseStatus(true, "Success", res.data);
     } catch (error) {
       return setApiResponseStatus(false, "Impossible de récupérer la liste des pays");
